8154110720f0c41b60e82bafd0d42841cf0c86ba
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-23 15:12:10 -0400

    Apply Dirty Bits code maid service

--- condition.rb
+++ condition.rb
@@ -1,14 +1,9 @@
 
 class Condition < ActiveRecord::Base
 
-  def data_getter(first, second, third)
-    data = HTTParty.get("http://api.wunderground.com/api/#{ENV['WUNDERGROUND_KEY']}/conditions/q/27253.json")
-    data[first][second][third]
-  end
-
   def build_condition(location)
-    data = HTTParty.get("http://api.wunderground.com/api/#{ENV['WUNDERGROUND_KEY']}/conditions/q/#{location}.json")
-    better_data = data["current_observation"]
+    api_data = HTTParty.get("http://api.wunderground.com/api/#{ENV['WUNDERGROUND_KEY']}/conditions/q/#{location}.json")
+    better_data = api_data["current_observation"]
     self.location = better_data["display_location"]["full"]
     self.temp_f = better_data["temp_f"]
     self.weather = better_data["weather"]
