46521058a5c091b48efb3356d7ae833413349908
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-23 10:08:15 -0400

    Add basic name: value to all table migrations

--- migrations.rb
+++ migrations.rb
@@ -1,22 +1,24 @@
+require './weather_report.rb'
 class WeatherReportMigration < ActiveRecord::Migration
   def change
     create_table :conditions do |t|
-      t.string :excetera
+      t.string :name
     end
 
     create_table :ten_days do |t|
-      t.string :excetera
+      t.string :name
     end
 
     create_table :dusk_dawns do |t|
-      t.string :excetera
+      t.string :name
     end
 
     create_table :current_alerts do |t|
-      t.string :excetera
+      t.string :name
     end
 
-    create_table :hurricans do |t|
-      t.string :excetera
+    create_table :hurricanes do |t|
+      t.string :name
     end
   end
+end
