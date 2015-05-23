4b7c505d69d3df362b9177f357f65766dae03735
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-23 10:50:52 -0400

    Add method to grab condition data from Wunderground using httparty.

--- condition.rb
+++ condition.rb
@@ -1,4 +1,7 @@
 class Condition < ActiveRecord::Base
 
-
+  def data_getter(first, second, third)
+    data = HTTParty.get("http://api.wunderground.com/api/#{ENV['WUNDERGROUND_KEY']}/conditions/q/27253.json")
+    data[first][second][third]
+  end
 end
